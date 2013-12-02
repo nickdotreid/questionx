76c66d0c44c2c4fc6675839110315dc89f301eed
Nick Reid <nickreid@nickreid.com>
2013-12-02 00:22:58 -0500

    made join url default url

--- questionx/questions/urls.py
+++ questionx/questions/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('questions.views',
 	url(r'^join','join'),
+	url(r'^$','join'),
 	url(r'^(?P<phone_number>\w+)','view'),
 )
