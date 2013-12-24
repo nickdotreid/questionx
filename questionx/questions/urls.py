23eb70b783a5439b0e22a3b4429fb7b9e3c6dfa1
Nick Reid <nickreid@nickreid.com>
2013-12-24 13:34:25 -0800

    added plus sign to url parser so that logins work

--- questionx/questions/urls.py
+++ questionx/questions/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('questions.views',
 	url(r'^join','join'),
 	url(r'^$','join'),
-	url(r'^(?P<phone_number>\w+)','view'),
+	url(r'^(?P<phone_number>\+\w+)','view'),
 )
