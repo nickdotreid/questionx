02339228a174ac7a64252f0143ac22700c69586f
Nick Reid <nickreid@nickreid.com>
2013-12-24 13:55:54 -0800

    added basic framework for questions CRUD

--- questionx/questions/views.py
+++ questionx/questions/views.py
@@ -8,7 +8,7 @@ from django import forms
 from localflavor.us.forms import USPhoneNumberField
 import phonenumbers
 
-from questions.models import Patient
+from questions.models import Patient, Question
 
 class JoinForm(forms.Form):
 	phone_number = USPhoneNumberField(required=True)
@@ -36,4 +36,24 @@ def view(request, phone_number):
 	patient = get_object_or_404(Patient, phone_number=phone_number)
 
 	return render_to_response('pages/list.html',{
+		'patient':patient,
 		},context_instance=RequestContext(request))
+
+def create(request, phone_number):
+	patient = get_object_or_404(Patient, phone_number=phone_number)
+
+	return render_to_response('questions/form.html',{
+		}, context_instance=RequestContext(request))
+
+def edit(request, phone_number, question_id):
+	question = get_object_or_404(Question, id=question_id)
+	patient = get_object_or_404(Patient, phone_number=phone_number)
+
+	return render_to_response('questions/form.html',{
+		}, context_instance=RequestContext(request))
+
+def delete(request, phone_number, question_id):
+	question = get_object_or_404(Question, id=question_id)
+	patient = get_object_or_404(Patient, phone_number=phone_number)
+
+	return HttpResponseRedirect(reverse(view, kwargs={'phone_number':patient.phone_number}))
