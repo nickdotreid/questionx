5da515afb43d85fd319aef51429e2350a9f93c35
Nick Reid <nickreid@nickreid.com>
2014-01-04 16:00:00 -1000

    added save recieved message reciever

--- questionx/smsmessages/recievers.py
+++ questionx/smsmessages/recievers.py
@@ -2,6 +2,15 @@ from django.dispatch import receiver
 
 from smsmessages.signals import smsmessage
 
+from smsmessages.models import Message
+
+@receiver(smsmessage)
+def save_message(sender, **kwargs):
+	phone = kwargs.get("phone")
+	message = kwargs.get("message")
+	m = Message(from_phone=phone, text=message)
+	m.save()
+
 @receiver(smsmessage)
 def stop_sending_to_phone(sender, **kwargs):
 	phone = kwargs.get("phone")
