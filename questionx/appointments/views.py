19f824e80430d84cc807cde96fe45a98d37793c5
Nick Reid <nickreid@nickreid.com>
2014-05-22 16:25:47 -0400

    added list view details

--- questionx/appointments/views.py
+++ questionx/appointments/views.py
@@ -60,8 +60,11 @@ def detail(request, appointment_id=None):
 	except:
 		return HttpResponseRedirect(reverse(list))
 	return render_to_response('appointments/detail.html',{
-		appointment: appointment,
+		'appointment': appointment,
 		},context_instance=RequestContext(request))
 
 def list(request):
-	return render_to_response('appointments/list.html',{},context_instance=RequestContext(request))
+	appointments = Appointment.objects.all()
+	return render_to_response('appointments/list.html',{
+		'appointments': appointments,
+		},context_instance=RequestContext(request))
